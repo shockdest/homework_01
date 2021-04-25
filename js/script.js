4ff590916882873d71230037f422ef05d1ab6a42
Paj Gabor <shockdest@gmail.com>
2021-04-25 18:42:15 +0200

    fix: JS to another file

---
+++ js/script.js
@@ -0,0 +1,110 @@
+ // Modal beállítása JS-el
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const message1 = document.getElementById('message');
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+const cname = document.getElementById('cname');
+const employees = document.getElementById('employees');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const emailValue = email.value.trim();
+  const message1Value = message1.value.trim();
+	const fnameValue = fname.value.trim();
+	const lnameValue = lname.value.trim();
+  const cnameValue = cname.value.trim();
+	const employeesValue = employees.value.trim();
+  const phonenumberValue = phonenumber.value.trim();
+
+	if(emailValue === '') {
+		setErrorFor(email, 'Please enter your email address');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Please enter a valid email address');
+	} else {
+		setSuccessFor(email);
+	}
+
+  if(message1Value === '') {
+		setErrorFor(message1, 'Please enter a message or question');
+	} else {
+		setSuccessFor(message1);
+	}
+	
+	
+	if(fnameValue === '') {
+		setErrorFor(fname, 'Please enter your first name');
+	} else {
+		setSuccessFor(fname);
+	}
+
+  if(lnameValue === '') {
+		setErrorFor(lname, 'Please enter your last name');
+	} else {
+		setSuccessFor(lname);
+	}
+	
+	if(cnameValue === '') {
+		setErrorFor(cname, 'Please enter your company name');
+	} else{
+		setSuccessFor(cname);
+	}
+  if(employeesValue === "noselect") {
+		setErrorFor(employees, 'Please select number of employees');
+	} else{
+		setSuccessFor(employees);
+	}
+
+  if(phonenumberValue === '') {
+		setErrorFor(phonenumber, 'Please enter a valid phone number');
+	} else{
+		setSuccessFor(phonenumber);
+	}
+
+}
+
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+  // formControl.style.color= 'red';
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
